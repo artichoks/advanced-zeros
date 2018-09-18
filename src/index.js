86e359d23c73b8b92edc30cc811ecc4674c7d199
Aksana Lissikh <LissikhA@iba>
2018-09-18 18:28:13 +0300

    Add getZerosCount

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let count = number;
+  const factors = factorize(base);
+  for (let qi of factors.keys()) {
+    let piQ = getQuantity(qi, number);
+    count = Math.floor(Math.min(count, piQ / factors.get(qi)));
+  }
+  return count;
+}
+
+function factorize(number) {
+  let factors = new Map();
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    let count = 0;
+    while (number % i === 0) {
+      count++;
+      number /= i;
+    }
+    if (count > 0) factors.set(i, count);
+  }
+  if (number > 1) factors.set(number, 1);
+  return factors;
+}
+
+function getQuantity(i, number) {
+  let count = 0;
+  while (number = Math.floor(number / i)) {
+    count += number;
+  }
+  return count;
 }
